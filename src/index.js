3868b625b7823d7b7f6af89b4f40c2285e2763c2
Vitor Campos <vitorp.ext@vr.com.br>
2019-08-17 21:54:43 -0300

    feat: :construction: include fontawesome and store structures

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/css/index.css';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import './styles/global';
+import 'bootstrap/dist/js/bootstrap';
+import './helpers/fontAwesome';
+
+import configureStore from './store';
+import theme from './styles';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById('root'),
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
